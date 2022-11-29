b5f49535ad5163fe22ecac3f3f16a512c7a3654a
Prakhar Mittal <mittalprakhar57@gmail.com>
2022-11-28 20:29:16 -0500

    Add note demo assembly file

--- BeepTest.asm
+++ BeepTest.asm
@@ -4,12 +4,7 @@
 
 ORG 0
 
-	; Get the switch values
-	IN     Switches
-	; Send to the peripheral
-	OUT    Note
-	
-	; Set mode
+	; Set to note mode
 	LOADI  2	
 	OUT    Mode
 	
@@ -17,31 +12,74 @@ ORG 0
 	LOADI  1
 	OUT	   Volume
 	
-	CALL   Delay
-	CALL   Delay
-	CALL   Delay
-	CALL   Delay
-	CALL   Delay
+	LOADI  &B001100000
+	STORE  Value
+	;OUT    NoteAddr
 	
-	; Get the switch values
-	IN     Switches
-	; Send to the peripheral
-	OUT    Note
+	; Octave = 3
+	LOADI 3
+	STORE Octave
+OctLoop:
+    ; Octave < 7
+	LOAD Octave
+	ADDI -6
+	JPOS EndOctLoop
 	
-	; Set mode
-	LOADI  2	
-	OUT    Mode
+	; Note = 0
+	LOADI 0 
+	STORE Note
 	
-	; Set volume
-	LOADI  2
-	OUT	   Volume
+	; Value = 0
+	LOAD Value
+	AND AndBitMask
+	STORE Value
+	
+NoteLoop:
+	; Note < 12
+	LOAD Note
+	ADDI -11
+	JPOS EndNoteLoop
+	
+	;Set Volume
+	IN Switches
+	AND VolBitMask
+	OUT Volume
+	
+	
+	; Play Note
+	LOAD Value
+	OUT Hex0
+	OUT LEDs
+	OUT NoteAddr
 	
 	CALL   Delay
 	CALL   Delay
-	CALL   Delay
-	CALL   Delay
-	CALL   Delay
+
 	
+	; Note++
+	LOAD Note
+	ADDI 1
+	STORE Note
+	; Value++ 
+	LOAD Value
+	ADDI 1
+	STORE Value
+	
+	JUMP   NoteLoop
+EndNoteLoop:
+	
+	; Octave++
+	LOAD Octave
+	ADDI 1
+	STORE Octave
+	; Value++
+	LOAD Value
+	ADDI &B100000
+	STORE Value
+	
+	JUMP OctLoop
+EndOctLoop:
+
 	; Do it again
 	JUMP   0
 	
@@ -53,6 +91,13 @@ WaitingLoop:
 	ADDI   -2
 	JNEG   WaitingLoop
 	RETURN
+	
+; LVs
+Octave: 	   DW  000
+Note:	   DW  000
+Value:	   DW  000
+AndBitMask:	DW &B111100000
+VolBitMask: DW &B000000011
 
 ; IO address constants
 Switches:  EQU 000
@@ -62,5 +107,9 @@ Hex0:      EQU 004
 Hex1:      EQU 005
 Mode: 	   EQU &H40
 TW:		   EQU &H41
-Note:      EQU &H42
+NoteAddr:      EQU &H42
 Volume:	   EQU &H43
+
+
+
+
